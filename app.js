18d5a834c8242c71c3af453f1de621016afe2374
root <sardorboboshov@gmail.com>
2021-08-07 22:59:45 +0500

    added

--- app.js
+++ app.js
@@ -173,7 +173,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Serving on ${port}`);
